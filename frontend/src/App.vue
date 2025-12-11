c9fd4716d8b6e638f1a3ec1c147be0754b753ee5
Mário Costa <mariocostaa6@gmail.com>
2025-12-11 05:29:13 +0000

    feat: create Vue.js application entry point and main App component

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container">
+    <Chat />
+  </div>
+</template>
+
+<script setup>
+import Chat from './components/Chat.vue'
+</script>
+
+<style scoped>
+.app-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
